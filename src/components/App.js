3b719077f64c1b74968e62638557d920c0afcdef
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-15 10:55:36 -0700

    Removed cow demo and added URL and image uploader; working

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,17 @@ import styles from './App.css';
 class App extends Component {
 
   state = {
-    content: 'The cow says moo',
-    cows: [],
-    cow: 'default',
-    url: 'https://www.beginningfarmers.org/wp-content/uploads/sites/4/2018/03/DSCN0028-550x264.jpeg'
+    content: 'This is a meme header',
+    url: 'https://i.redd.it/awfefcu0c9g11.jpg'
   };
 
+  handleBackgroundChoose = (url = '') => {
+    this.setState({ url });
+  }
 
   render() {
 
-    const { content, cows, cow, url } = this.state;
+    const { content, url } = this.state;
 
     return (
       <main className={styles.app}>
@@ -22,27 +23,43 @@ class App extends Component {
           <h2>SUPER COOL MEME STUFF</h2>
         </section>
 
-        <section className="cow-say">
-            <span ref ={node => this.image = node}>
-              <CowSay content={content} cow={cow} url={url}/>
-            </span>
+        <section>
+          <Background url={url} onChoose={this.handleBackgroundChoose}/>
+          <Meme content={content} url={url} onChange={this.handleContentChange}/>
         </section>
       </main>
     );
   }
 }
 
-function CowSay({ content, cow, url }) {
-  
-  const cowSaid = cowsay.say({
-    text: content || ' ',
-    f: cow
-  });
+
+
+function Meme({ url }) {
 
   return (
     <Fragment>
-      <pre style={{ background:`url(${url}) no-repeat`}}>{cowSaid}</pre>
+      <div id="background" style={{ background:`url(${url}) no-repeat`}}></div>
     </Fragment>
+  );
+}
+
+function Background({ url, onChoose }) {
+  return (
+    <label>
+      Select a meme by pasting a URL or uploading an image:<br/>
+      <input size="100" value={url} onChange={({ target }) => onChoose(target.value)}/><br/>
+      <input type="file" onChange={({ target }) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(target.files[0]);
+        reader.onload = () => onChoose(reader.result);
+      }}/>
+    </label>
+  )
+}
+
+function headerContent({ content, onChange }) {
+  return (
+    <input value={content} onChange={({ target }) => onChange(target.value)}/>
   );
 }
 
